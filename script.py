5fa364d4500127b2446a6dc69f59384f4085b2e6
Waneyok <waneyok@yandex.ru>
2026-08-11 20:18:55 +0300

    update from css_selector to xpath value

--- script.py
+++ script.py
@@ -11,15 +11,27 @@ def test_get_count_students():
         # need_link = browser.find_element(
         #     By.LINK_TEXT, "ИНФОРМАЦИЯ ДЛЯ ПОСТУПАЮЩИХ"
         # )
-        need_link = browser.find_element(By.CSS_SELECTOR, "div.nabor2 p:nth-child(3) a:nth-child(1)")
+        # need_link = browser.find_element(By.CSS_SELECTOR, "div.nabor2 p:nth-child(3) a:nth-child(1)")
+        need_link = browser.find_element(By.XPATH, "//*[contains(text(), 'Информация')]")
         need_link.click()
-        tables = browser.find_elements(By.CSS_SELECTOR, "table.MsoTableGrid")
+        
+        # tables = browser.find_elements(By.CSS_SELECTOR, "table.MsoTableGrid")
+        tables = browser.find_elements(By.XPATH, "//table[contains(@class, 'MsoTableGrid')]")
+        
+        # tb_element = tables[1].find_element(
+        #     By.CSS_SELECTOR, "tr:nth-child(3) td:nth-child(3)"
+        # )
         tb_element = tables[1].find_element(
-            By.CSS_SELECTOR, "tr:nth-child(3) td:nth-child(3)"
+            By.XPATH, "./tr[3]/td[3]"
         )
+        
+        # tb_spec_name = tables[1].find_element(
+        #     By.CSS_SELECTOR, "tr:nth-child(3) td:nth-child(1)"
+        # )
         tb_spec_name = tables[1].find_element(
-            By.CSS_SELECTOR, "tr:nth-child(3) td:nth-child(1)"
+            By.XPATH, "./tr[3]/td[1]"
         )
+        
         print(f"Количество заявлений = {tb_element.text} по специальности '{tb_spec_name.text}'")
     except Exception as e:
         print(f"Произошла ошибка при выполнении теста: {e}")
